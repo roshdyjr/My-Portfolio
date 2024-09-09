98f5ec639cb63d3fb6be8d847d77cbfced2ec094
roshdyjr <ahmedrush96@gmail.com>
2024-09-09 09:31:13 +0300

    fixed projects image

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
